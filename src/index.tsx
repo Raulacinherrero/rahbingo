a712a2f4b0c07f9ff45263a538330e8d19d7ab57
Raulacinherrero <acin.raul@gmail.com>
2023-05-16 00:05:18 +0200

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+import App from './components/App/App';
+import Navbar from './components/Navbar/Navbar';
+
+ReactDOM.render(
+  <>
+    <Navbar />
+    <App />
+  </>,
+  document.getElementById('root')
+);
