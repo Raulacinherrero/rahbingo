80d677c7935ca47627b72d126bea8dfbbcced2ba
Raulacinherrero <acin.raul@gmail.com>
2023-06-02 05:37:14 +0200

    Ganadores con estilo

--- src/components/ShowWinners/ShowWinners.tsx
+++ src/components/ShowWinners/ShowWinners.tsx
@@ -1,53 +1,63 @@
 import React from "react";
+import "./show-winners.scss";
 
 const ShowWinners = ({ DatosPartida }) => {
 
-  const getJugadorNombre = (idJugador: string): string | undefined => {
-    const jugador = DatosPartida.ListaJugadores.find(
-      (jugador) => jugador.idJugador === idJugador
+    const getJugadorNombre = (idJugador: string): string | undefined => {
+        const jugador = DatosPartida.ListaJugadores.find(
+            (jugador) => jugador.idJugador === idJugador
+        );
+        return jugador?.nombreJugador;
+    };
+
+    return (
+        <div className="winners-container">
+            <h1 className="winners-title">¡¡¡ENHORABUENA GANADORES!!!</h1>
+            <h2 className="winners-subtitle">Ganadores de Línea:</h2>
+            <ul className="winners-list">
+                {DatosPartida.GanadoresLinea.map((ganador, index) => (
+                    <>
+                        <p key={index} className="winners-player">{getJugadorNombre(ganador)}</p>
+                        <p className="winners-player">Hola</p>
+                    </>
+                ))}
+            </ul>
+
+            <h2 className="winners-subtitle">Ganadores de Bingo:</h2>
+            <ul className="winners-list">
+                {DatosPartida.GanadoresBingo.map((ganador, index) => (
+                    <p key={index} className="winners-player">{getJugadorNombre(ganador)}</p>
+                ))}
+            </ul>
+
+            {DatosPartida.DespistadosLinea.length > 0 || DatosPartida.DespistadosBingo.length > 0 ? (
+                <h1 className="winners-title">¡¡¡TENEMOS DESPISTADOS!!!</h1>
+            ) : null}
+
+
+            {DatosPartida.DespistadosLinea.length > 0 && (
+                <>
+                    <h2 className="winners-subtitle">Despistados de Línea:</h2>
+                    <ul className="winners-list">
+                        {DatosPartida.DespistadosLinea.map((despistado, index) => (
+                            <p key={index} className="winners-player">{getJugadorNombre(despistado)}</p>
+                        ))}
+                    </ul>
+                </>
+            )}
+
+            {DatosPartida.DespistadosBingo.length > 0 && (
+                <>
+                    <h2 className="winners-subtitle">Despistados de Bingo:</h2>
+                    <ul className="winners-list">
+                        {DatosPartida.DespistadosBingo.map((despistado, index) => (
+                            <p key={index} className="winners-player">{getJugadorNombre(despistado)}</p>
+                        ))}
+                    </ul>
+                </>
+            )}
+        </div>
     );
-    return jugador?.nombreJugador;
-  };
-
-  return (
-    <div>
-      <h2>Ganadores de Línea:</h2>
-      <ul>
-        {DatosPartida.GanadoresLinea.map((ganador, index) => (
-          <li key={index}>{getJugadorNombre(ganador)}</li>
-        ))}
-      </ul>
-
-      <h2>Ganadores de Bingo:</h2>
-      <ul>
-        {DatosPartida.GanadoresBingo.map((ganador, index) => (
-          <li key={index}>{getJugadorNombre(ganador)}</li>
-        ))}
-      </ul>
-
-      {DatosPartida.DespistadosLinea.length > 0 && (
-        <div>
-          <h2>Despistados de Línea:</h2>
-          <ul>
-            {DatosPartida.DespistadosLinea.map((despistado, index) => (
-              <li key={index}>{getJugadorNombre(despistado)}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-
-      {DatosPartida.DespistadosBingo.length > 0 && (
-        <div>
-          <h2>Despistados de Bingo:</h2>
-          <ul>
-            {DatosPartida.DespistadosBingo.map((despistado, index) => (
-              <li key={index}>{getJugadorNombre(despistado)}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
-  );
 };
 
 export default ShowWinners;
